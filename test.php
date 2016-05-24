4ef8bf28905ba505230167b68550db7e339edb6d
robmro27 <robert.mroz1985@gmail.com>
2016-05-24 16:56:32 +0200

    testing query for limited orders

--- test.php
+++ test.php
@@ -3,8 +3,41 @@
 include 'app/Mage.php';
 Mage::app();
 
+$nextWeekdays = Mage::helper('polcodeshipping')->nextWeekDates();
+
+$ordersCollection = Mage::getModel('sales/order')
+        ->getCollection()
+        ->addFieldToFilter('mainpolcode_delivery_date', array('from' => reset($nextWeekdays),'date' => true,))
+        ->addFieldToFilter('polcode_delivery_date', array('to' => end($nextWeekdays), 'date' => true,))
+        ->addFieldToFilter('polcode_shipping_id', array('notnull' => true))
+        ;
 
 
+echo '<pre>';
+print_r($ordersCollection
+            ->getSelect()
+            ->columns('count(*) as qty')
+            ->group('polcode_shipping_id'));
+echo '</pre>';
+
+foreach ($ordersCollection as $result) {
+    
+    
+    
+}
+
+die;
+
+foreach ($ordersCollection as $collection) {
+    echo '<pre>';
+    print_r($collection->getData());
+    echo '</pre>';
+}
+
+echo '<pre>';
+print_r($nextWeekdays);
+echo '</pre>';
+die;
 
 // set default 
     $shippingModel = Mage::getModel('polcodeshipping/shipping');
